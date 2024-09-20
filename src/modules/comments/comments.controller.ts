e72f95a9bbfbfb9740c217b383f1232de53e8414
Ira Marysheva <380934581587a@gmail.com>
2024-09-20 15:04:03 +0300

    add connect with all table, Add CRUD and like route

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,33 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
-  @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentsService.create(createCommentDto);
+  @Post('/post/:idPost')
+  @UseGuards(JwtAuthGuard)
+  create(
+    @Req() req,
+    @Param('idPost') idPost: string,
+    @Body() createCommentDto: CreateCommentDto,
+  ) {
+    return this.commentsService.create(+req.user.id, +idPost, createCommentDto);
   }
-
-  @Get()
-  findAll() {
-    return this.commentsService.findAll();
+  // all comment for one post
+  @Get('/post/:idPost')
+  @UseGuards(JwtAuthGuard)
+  findAll(@Param('idPost') idPost: string) {
+    return this.commentsService.findAll(+idPost);
   }
-
+  // get one comment
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.commentsService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.update(+id, updateCommentDto);
   }
 
+  @Patch('like/:id')
+  @UseGuards(JwtAuthGuard)
+  likePost(@Param('id') id: string) {
+    return this.commentsService.likePost(+id);
+  }
+
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.commentsService.remove(+id);
   }
