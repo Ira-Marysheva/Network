7ae4c3583983e241e523fe140d85e96398f67b1e
Ira-Marysheva <380934581587a@gmail.com>
2024-11-25 16:03:21 +0200

    add basic work with broker message(RabbitMQ) (without answer)

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller,  } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // function when user was created (for RabitMQ) 
+  @EventPattern('user_created')
+  handleRegisterUser(@Payload() user){
+    return this.appService.confirmUserRegistration(user.user, user.token);
+  }
 }
